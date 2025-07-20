df6bffc0b8b1d38301049a1b5a6526126b525ad3
DIVINE TANGU <dtangu79@gmail.com>
2025-07-20 00:58:14 +0000

    Setup github action

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './LIBERTIS-HOTEL-MANAGEMENT-SYSTEM',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
